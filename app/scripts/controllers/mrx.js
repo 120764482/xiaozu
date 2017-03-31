d8b5896e0bd45033ee91995152464f175164e9e4
120764482 <https://github.com/120764482/xiaozu.git>
2017-03-31 08:53:51 +0800

    3.31-5

--- app/scripts/controllers/mrx.js
+++ app/scripts/controllers/mrx.js
@@ -8,36 +8,60 @@
  * Controller of the lytappApp
  */
 angular.module('lytappApp')
-.controller('mrxCtrl',["$scope","$http",function ($scope,$http) {
+.controller('mrxCtrl',["$scope","$http","$filter",function ($scope,$http,$filter) {
 	$scope.pageNow=1;
   	$scope.page=5;
 	$scope.totalPage=0;	
-	$scope.aa=false;
+	//$scope.aa=false;
 //全选
 	$scope.iCkeck=false;
 	$scope.gb=false;	
 	$scope.sc=false;
 	$scope.checkAll=function(){
-		$scope.iCkeck=!$scope.iCkeck;
+		//$scope.iCkeck=!$scope.iCkeck;  // 联动全选
+		// 这里选择了全部客户，就把所有的客户的id存起来
+		 
+
 	}
-	$scope.cli=function(){
-		$scope.aa=!$scope.aa;   
+	$scope.cli=function(aa,bb){
+//		$scope.aa=!$scope.aa;   
+         if(aa==true){
+         	$scope.nowID=bb;
+         	
+         }
 	}
 //批量分配
 	   	
 		$scope.fp=function(){ 
-			if(!$scope.iCkeck&&$scope.aa!=true){
-		 		// alert("请先选择数据");		 		 
-		 		 $scope.zhez=true;
+			if($scope.nowID){
+				 // 判断当前客户时候选择
+				 $scope.gb=true;
+			}else{
+				$scope.zhez=true;
 					$(".zhe").text("请先选择数据");
 					$scope.shanshan=function(){
 						$scope.zhez=false;
 				};
-		 		 
-		   }else{			
-				$scope.gb=true; 
 			}
+			
+			
+			
+//			if(!$scope.iCkeck&&$scope.aa!=true){
+//		 		// alert("请先选择数据");		 		 
+//		 		 $scope.zhez=true;
+//					$(".zhe").text("请先选择数据");
+//					$scope.shanshan=function(){
+//						$scope.zhez=false;
+//				};
+//		 		 
+//		   }else{			
+//				$scope.gb=true; 
+//			}
 		}
+
+		
+		
+		
 //关闭
 	 
 	$scope.close=function(){
@@ -66,14 +90,55 @@ angular.module('lytappApp')
 		})
 	 }
 //查询	 
-	  $scope.cha=function(){
-	  	$http({
-	  		url: "http://47.88.16.225:411/kehu?mingcheng="+$scope.omingcheng,
-	  		method:"get",
-	  	}).then(function(data){
-	  		$scope.arr=data.data
-	  	})
-	  }
+	 var arr1=[
+            ['储备客户','潜在客户','成交客户','目标客户'],
+            [''],
+		        ['初期','中期','处理异议'],
+		        ['个人客户','团队客户'],
+		        ['已沟通','未沟通']
+	];
+	$('#ss').change(function(){
+		var a=$('#ss').val();
+		fn(a)
+	})	
+	
+	//ww
+	function fn(x){
+		$('#cs').empty();
+		for(var i=0;i<arr1[x-2].length;i++){
+				$('#cs').append('<option value='+arr1[x-2][i]+' ng-click="show()">'+arr1[x-2][i]+'</option>')
+		};
+	}
+	
+	//查询
+	$scope.show = function(e){
+		alert(e);
+	}
+	
+	$scope.btn=function(e){
+		$scope.classes = angular.element(".m_main_bottom-top-right_b_r_x").val();
+		$scope.context = angular.element(".m_main_bottom-top-right_b_r_g").val();
+		if($scope.classes == 1){
+			$scope.classes = "搜索";
+		}else if($scope.classes == 2){
+			$scope.classes = "jieduan";
+		}else if($scope.classes == 3){
+			$scope.classes = "xingming";
+		}else if($scope.classes == 4){
+			$scope.classes = "yingxiaojieduan";
+		}else if($scope.classes == 5){
+			$scope.classes = "leixing";
+		}else if($scope.classes == 6){
+			$scope.classes = "biaoqian";
+		}
+		var classes = $scope.classes;
+		var context = $scope.context;
+		$scope.objStr = '{"'+classes+'":"'+context+'"}';
+		var objStr = $scope.objStr;
+		$scope.obj = JSON.parse(objStr);
+		$scope.arr = $filter("filter")($scope.dataArr, $scope.obj);
+	}	
+	
 	//编辑
 			$scope.bbbb=false;
 			$scope.xiugai=function(e){
@@ -130,20 +195,27 @@ angular.module('lytappApp')
 			$scope.guan=function(){
 				$scope.bbbb=false;
 			}	 
-	  
-//分页
-//	$http({
-//		url:"http://47.88.16.225:411/kehu",
-//		method:"get"
-//	}).then(function(req){
-////		$scope.pageNow=1;
-////		$scope.totalPage=Math.ceil(req.data.length/$scope.page);
-////		$scope.tiao = req.data.length;
-//		$scope.arr = req.data;
-////		$scope.arr=$scope.arr.slice(($scope.pageNow-1)*$scope.page,$scope.pageNow*$scope.page);
-//	},function(){
-//		console.log("请求失败");
-//	})
+////获取列表
+   	$http({
+   		url:"http://47.88.16.225:411/kehu/",
+   		method:"get"
+   	}).then(function(data){
+   		
+   		var cusArr = [];
+		var data2 = data.data;
+		for(var i=0; i<data2.length; i++){
+			if(data2[i].zhixingren == "public"){
+		      cusArr.push(data2[i]);
+		    }
+		}
+		$scope.pageNow=1;
+		$scope.totalPage=Math.ceil(cusArr.length/$scope.page);
+		$scope.tiao = cusArr.length;
+		$scope.arr = cusArr;
+		$scope.arr=$scope.arr.slice(($scope.pageNow-1)*$scope.page,$scope.pageNow*$scope.page);
+//		$scope.arr = cusArr;
+//		console.log(cusArr);
+   	})	  
 //上一页	
 	$scope.prev=function(){
 		if($scope.pageNow<=1){
@@ -195,34 +267,90 @@ angular.module('lytappApp')
 				$scope.tiao = cusArr.length;
 				$scope.arr = cusArr;
 				$scope.arr=$scope.arr.slice(($scope.pageNow-1)*$scope.page,$scope.pageNow*$scope.page);
-			})
-			
-		}
-		
+			})			
+		}		
 	}
-//
-////获取列表
-   	$http({
-   		url:"http://47.88.16.225:411/kehu/",
-   		method:"get"
-   	}).then(function(data){
-   		
-   		var cusArr = [];
-		var data2 = data.data;
-		for(var i=0; i<data2.length; i++){
-			if(data2[i].zhixingren == "public"){
-		      cusArr.push(data2[i]);
-		    }
-		}
-		$scope.pageNow=1;
-		$scope.totalPage=Math.ceil(cusArr.length/$scope.page);
-		$scope.tiao = cusArr.length;
-		$scope.arr = cusArr;
-		$scope.arr=$scope.arr.slice(($scope.pageNow-1)*$scope.page,$scope.pageNow*$scope.page);
-//		$scope.arr = cusArr;
-//		console.log(cusArr);
-   	})
+// 分配人员
+$scope.sel=function(sele){
+	$scope.nowBe=sele;
+}
 
+//保存
+$scope.sels={};
+   $scope.chu=function(){
+   		$scope.gb=false;
+   		// $scope.nowBe  这是分配给那个人的id
+   		console.log($scope.nowBe,$scope.nowID);
+     		$http({
+			    url: "http://47.88.16.225:411/kehu/"+$scope.nowID,
+			    method: "put",
+			    data:{zhixingren:$scope.nowBe}
+		       }).then(function(data) {
+			          console.log(data)
+		     })
+
+   }
+   
+ //放入客户管理  
+   $scope.kehu = function(obj){
+		$http({
+			url:"http://47.88.16.225:411/kehu/" + obj.id,
+			method:"put",
+			data: {
+				"duiyingkehu": obj.duiyingkehu,
+				"lianxineirong": obj.lianxineirong,
+				"lianxiriqi":  obj.lianxiriqi,
+				"lianxiren":  obj.lianxiren,
+				"zhixingren":  "a123456",
+				"tel": obj.tel
+			}
+		}).then(function(req){
+			 $scope.zhez=true;
+//					$(".zhe").text("success");
+//					$scope.shanshan=function(){
+//						$scope.zhez=false;
+//				};
+			alert("success");        
+			location.reload();
+		},function(){
+			console.log("修改失败！");
+		})
+	}
+   
+   //手机验证
+		var dianhua = /^((\+)?86|((\+)?86)?)0?1[3458]\d{9}$/; 
+		$("#dianhua").blur(function() {
+			if($(this).val().match(dianhua)) {
+				$(this).next().html('√').css("color", "green");
+			} else {
+				$(this).next().html('请输入正确格式').css("color", "red");
+			}
+		})
+	//邮箱验证
+		var email = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+		$("#email").blur(function() {
+			if($(this).val().match(email)) {
+				$(this).next().html('√').css("color", "green");
+			} else {
+				$(this).next().html('请输入正确格式').css("color", "red");
+			}
+		})	
+		
+		
+		
+//分页
+//	$http({
+//		url:"http://47.88.16.225:411/kehu",
+//		method:"get"
+//	}).then(function(req){
+////		$scope.pageNow=1;
+////		$scope.totalPage=Math.ceil(req.data.length/$scope.page);
+////		$scope.tiao = req.data.length;
+//		$scope.arr = req.data;
+////		$scope.arr=$scope.arr.slice(($scope.pageNow-1)*$scope.page,$scope.pageNow*$scope.page);
+//	},function(){
+//		console.log("请求失败");
+//	})		
    //放入客户管理
 //	$scope.gonghai = function(id, obj){
 //		$http({
@@ -241,44 +369,6 @@ angular.module('lytappApp')
 //		},function(){
 //			console.log("修改失败！");
 //		})
-//	}
-//保存
-   $scope.chu=function(){
-// 		$scope.gb=false;
-// 		$http({
-// 			url:"http://47.88.16.225:411/kehu/"
-// 			,method:"put"
-// 		}).then(function(req){
-// 			
-// 			
-// 		},function(){
-// 			console.log("请求失败")	
-// 		})
-   }
+//	}		
    
- //放入客户管理  
-   $scope.kehu = function(obj){
-		$http({
-			url:"http://47.88.16.225:411/kehu/" + obj.id,
-			method:"put",
-			data: {
-				"duiyingkehu": obj.duiyingkehu,
-				"lianxineirong": obj.lianxineirong,
-				"lianxiriqi":  obj.lianxiriqi,
-				"lianxiren":  obj.lianxiren,
-				"zhixingren":  "a123456",
-				"tel": obj.tel
-			}
-		}).then(function(req){
-			 $scope.zhez=true;
-					$(".zhe").text("success");
-					$scope.shanshan=function(){
-						$scope.zhez=false;
-				};
-			//alert("success");        
-			location.reload();
-		},function(){
-			console.log("修改失败！");
-		})
-	}
 }]);
